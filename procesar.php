e82130eef4e736bf5a9f2a92a0bb841d6af50181
Crisnol <cnolasco082@gmail.com>
2025-11-28 01:06:47 +0000

    cambios en formulario y se agrega procesar.php

---
+++ procesar.php
@@ -0,0 +1,22 @@
+<?php
+        $nombre_post = $_POST['Nombre'];
+        $comentario_post = $_POST['Comentario'];
+        
+
+        $conexion = mysqli_connect("sql100.infinityfree.com","if0_40528293","cxGeW6nWfTTKE","if0_40528293_usuarios");
+        
+        if(!$conexion){
+            echo "Error no se pudo conectar a MySQL.";
+            echo "Errno de depuracion: " . mysqli_connect_errno();
+            echo "error de depuracion: " . mysqli_connect_error();
+            exit;
+        }
+
+        $sql = "INSERT INTO visitantes (nombre, comentario) VALUES ('$nombre_post','$comentario _post')";
+        if(mysqli_query($conexion, $sql)){
+            echo "Registro insertado exitosamente";
+        }else{
+            echo "Error: no se pudo realizar el registro" , "<br>" , mysqli_error($conexion);
+        }
+        
+?>
